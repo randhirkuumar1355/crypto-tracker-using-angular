ec412e15ef45d0fcb4981cf0a58fe80583c45a82
Randhir Kumar <randhirk413@gmail.com>
2023-10-06 10:50:24 +0530

    Updated to Angular Version 16
    Updated to Angular Version 16

--- src/app/pages/coin-list/coin-list.component.ts
+++ src/app/pages/coin-list/coin-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatLegacyPaginator as MatPaginator } from '@angular/material/legacy-paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { Router } from '@angular/router';
 import { CryptoService } from 'src/app/services/crypto.service';
 import { CurrencyService } from 'src/app/services/currency.service';
---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://api.coingecko.com/api/v3/coins/'
+};
